53294ce0d2d1184799efeb35304b93bc09d18d46
Bayram Emiraliev <bayram.kad@gmail.com>
2020-10-17 20:04:00 +0300

    Обязательное ДЗ

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 let money = +prompt("Ваш доход за месяц?");
-
+const addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую (пример: Квартплата, проездной, кредит)");
+console.log(addExpenses.split(" "));
+const deposit = confirm("Есть ли у вас депозит в банке?");
 const expenses = prompt("Введите обязательные расходы");
 const emout = +prompt("Во сколько это обойдется?");
 const expenses2 = prompt("Введите обязательные расходы");
@@ -12,7 +14,14 @@ const bud = mission / budgetMonth;
 console.log("Ваш буджет на месяц: ", budgetMonth);
 console.log(`Вы соберете ${mission} руб через ${Math.ceil(bud)} месяцев`);
 
-let budgetDay = 30000;
+let budgetDay = budgetMonth;
+console.log(Math.floor(budgetDay))
+
+/*
+ Поправить budgetDay учитывая бюджет на месяц, 
+ а не месячный доход. Вывести в консоль  округлив 
+ в меньшую сторону 
+ */
 
 if (budgetDay >= 1200){
     console.log("У вас высокий уровень дохода");
