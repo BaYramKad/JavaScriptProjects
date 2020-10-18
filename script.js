8ed029dee71ec1c828bfee0fde19ea5924fd74cf
Bayram Emiraliev <bayram.kad@gmail.com>
2020-10-18 14:43:46 +0300

    Функции

--- script.js
+++ script.js
@@ -1,30 +1,42 @@
 
-let money = +prompt("Ваш доход за месяц?");
-const addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую (пример: Квартплата, проездной, кредит)");
-console.log(addExpenses.split(" "));
-const deposit = confirm("Есть ли у вас депозит в банке?");
-const expenses = prompt("Введите обязательные расходы");
-const emout = +prompt("Во сколько это обойдется?");
-const expenses2 = prompt("Введите обязательные расходы");
-const emout2 = +prompt("Во сколько это обойдется?");
-const exp = emout + emout2;
-const budgetMonth = money - exp;
-let mission = 4000000;
-const bud = mission / budgetMonth;
-console.log("Ваш буджет на месяц: ", budgetMonth);
-console.log(`Вы соберете ${mission} руб через ${Math.ceil(bud)} месяцев`);
+let money = +prompt("Ваш месячный доход", "150000" );
+let income = "Фриланс";
+let deposit = true;
+let mission = 4000000; 
 
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit); 
+console.log("Ваш доход за месяц: ", money); 
 
-let budgetDay = budgetMonth / 30;
-console.log("Бюджет на один день: ", Math.floor(budgetDay), "руб");
+const expenses = prompt("Введите обязательные расходы, например (Ремонт, Дом, Рестораны)");
+const emout = +prompt("Во сколько это обойдеться?", "25000");
+const expenses2 = prompt("Введите обязательные расходы например (Спортзал, Кино)");
+const emout2 = +prompt("Во сколько это обойдеться?", "25000");
+const addExpenses = expenses + expenses2;
+console.log(addExpenses.split(", "));
 
-if (budgetMonth >= 1200){
-    console.log("У вас высокий уровень дохода");
-} else if (budgetMonth >= 600 && budgetMonth <= 1200){
-    console.log("У вас средний уровень дохода");
-} else if (budgetMonth <= 600 && budgetMonth >= 0){
-    console.log("К сожалению у вас уровень дохода ниже среднего");
-} else if (budgetMonth < 0){
-    console.log("Что то пошло не так");
+function getExpensesMonth(emout, emout2){
+    const exp = emout + emout2;
+    return exp;
+}
+let resultExpenses = getExpensesMonth (emout, emout2);
+console.log("Расходы за месяц: ", resultExpenses);
+
+function getAccumulatedMonth(income, costs){
+    const result = income - costs;
+    return result;
+}
+let accumulatedMonth = getAccumulatedMonth(money, resultExpenses);
+console.log("Ваш бюджет на месяц: ", accumulatedMonth);
+
+function getTargetMonth(mission, income){
+    const resultTarget = mission / income;
+    return resultTarget;
 }
 
+let getTarget = getTargetMonth(mission, accumulatedMonth);
+console.log(`Вы соберете ${mission} руб через ${Math.ceil(getTarget)} месяцев`);
+
+const resultgetDay = accumulatedMonth / 30;
+console.log("Бюджет на один день: ", Math.floor(resultgetDay), "руб");
