b40d43a72f09a704fc017f0c164f514b5d2b61e3
Bayram Emiraliev <bayram.kad@gmail.com>
2020-11-08 10:51:22 +0300

    Передвижение блока

--- script.js
+++ script.js
@@ -24,24 +24,33 @@ document.addEventListener("DOMContentLoaded", function() {
         div.style.position = "absolute";
     };
     const block = new DomElement();
+    
     function foo(callback){
         DomElement.call(this, arguments);
         callback();
-        
-        document.addEventListener("keydown", function (event){
-            let key = event.key;
+        document.addEventListener("keydown", keyDownfunc, false);
+        function keyDownfunc(event){
+            const div = document.querySelector("div");
+            var cs = window.getComputedStyle(div);
+            var left = parseInt(cs.transform);
+            var top = parseInt(cs.transform);
+            var right = parseInt(cs.transform);
+            var down = parseInt(cs.transform);
+            let key = event.which;
             console.log('key: ', key);
-            if (key === "ArrowUp"){
-                div.style.transform = "translateY(-10)";
-            } else if (key === "ArrowDown"){
-                div.style.transform = "translateY(10px)";
-            } else if (key === "ArrowRight"){
-                div.style.transform = "translateX(10px)";
-            } else if (key === "ArrowLeft"){
-                div.style.transform = "translateX(-10px)";
+            if (event.which === 40 || left > 0){
+                div.style.transform = 10 + "px";
+            } 
+            if (event.which === 38 || top > 0){
+                div.style.transform = 10 + "px";
+            } 
+            if (event.which === 39 || right > 0){
+                div.style.transform = 10 + "px";
+            } 
+            if (event.which === 37 || down > 0){
+                div.styletransform = 10 + "px";
             }
-            callback();
-        });
+        };
     }
     foo(block.block);
 });
