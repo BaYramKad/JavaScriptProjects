97730737e9ae9ad13ee72b3a2a0f9a67075902a3
Bayram Emiraliev <bayram.kad@gmail.com>
2020-11-09 16:30:08 +0300

    Наследование

---
+++ script.js
@@ -0,0 +1,21 @@
+class First{
+    constructor(){
+        this.hello;
+    }
+    hello(){
+        console.log("Привет я метод родителя!");    
+    }
+}
+const first = new First();
+
+class Second extends First {
+    constructor(){
+        super();
+    }
+    hello(){
+        first.hello();
+        console.log("А я наследуемый метод!");
+    }
+}
+const second = new Second();
+second.hello();
